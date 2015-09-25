69cf1bec17bde27e37ba98aa0cb6145efeace8bf
Phil Sofia <phil.sofia@gmail.com>
2015-09-25 17:16:27 -0400

    Working with all data

--- models/parser.rb
+++ models/parser.rb
@@ -1,6 +1,9 @@
 
 
 module Parser
+
+  DATES = ["1136073600", "1167609600", "1199145600", "1230768000", "569287440000"]
+
   CAPACITIES = { "neversink" => 34_900, "cannonsville" => 95_700,
     "schoharie" => 17_600, "pepacton" => 140_200, "rondout" => 49_600 }
 
@@ -17,12 +20,14 @@ module Parser
   end
 
   def self.load_json
-    data = open("https://data.cityofnewyork.us/resource/zkky-n5j3.json").read
+    data = open("https://data.cityofnewyork.us/resource/zkky-n5j3.jso://data.cityofnewyork.us/resource/zkky-n5j3?$limit=50000").read
     data = JSON.parse(data)
   end
 
   def self.get_data_objects(data, resevoir)
     data.each_with_object([]) do |(row), data_points|
+      # continue if !DATES.include?(row[resevoir + '_date'])
+
       date_field = Time.at(row[resevoir + '_date'])
       level = row[resevoir + '_storage'].to_f / CAPACITIES[resevoir] * 100
 
