a1c8738081b871942b7cfd7c1b19f2e5c7bfa16d
André <email": null>
2015-09-25 17:56:32 -0400

    final

--- runner.rb
+++ runner.rb
@@ -17,7 +17,7 @@ def vizualize(reservoirs)
     # binding.pry
     puts reservoirs[0].data[i].date
     puts AsciiCharts::Cartesian.new([[reservoirs[0].name, reservoirs[0].data[i].level], [reservoirs[1].name, reservoirs[1].data[i].level], [reservoirs[2].name, reservoirs[2].data[i].level], [reservoirs[3].name, reservoirs[3].data[i].level],[reservoirs[4].name, reservoirs[4].data[i].level],["Maximum + 30%", 130]], :bar => true, :title => "NYC Reservoir System Levels").draw
-    sleep(0.05)
+    sleep(0.01)
     system("clear")
   end
 end
